634eae177145cf90903ec3aea8aaabb72def7f25
lukascivil <lukas_civil@hotmail.com>
2019-09-14 20:58:05 -0300

    fix: mjml compiler

--- lib/elixir_with_mjml_web/controllers/mjml_controller.ex
+++ lib/elixir_with_mjml_web/controllers/mjml_controller.ex
@@ -5,23 +5,48 @@ defmodule ElixirWithMjmlWeb.MjmlController do
     render(conn, "index.json")
   end
 
-  # defp compile(message) do
-  #   case :os.cmd(:"cat << EOF | mjml -i #{message}") do
+  # defp compileMjml(message) do
+  #   case :os.cmd(:"cat #{path}/mjml.sh") do
   #     {html, 0} -> html
   #     _         -> raise "Error MJML exited with non zero status"
   #   end
   # end
 
   def compileMjml(conn, %{"mjmlTemplate" => mjmlTemplate}) when is_binary(mjmlTemplate) do
-    with {html, 0} <- :os.cmd("cat << EOF | mjml -i #{mjmlTemplate}") |> IO.inspect do
+    with path <- :code.priv_dir(:elixir_with_mjml), 
+      {html, 0} <- :os.cmd(:"#{path}/mjml.sh '<mjml><mj-body><mj-section></mj-section></mj-body></mjml>'") |> IO.inspect do
         render(conn, "index.json", message: html)
     else
         _error -> raise "Error MJML exited with non zero status"
     end
   end
 
+  # def compileMjml(conn, %{"mjmlTemplate" => mjmlTemplate}) when is_binary(mjmlTemplate) do
+  #   with path <- :code.priv_dir(:elixir_with_mjml), 
+  #     {html, 0} <- :os.cmd(:"cat #{path}/mjml.sh") |> IO.inspect do
+  #       render(conn, "index.json", message: html)
+  #   else
+  #       _error -> raise "Error MJML exited with non zero status"
+  #   end
+  # end
+
   # def compileMjml(conn, %{"mjmlTemplate" => mjmlTemplate} = params) do
   #   render(conn, "index.json", message: compile(mjmlTemplate))
   # end
 
 end
+
+
+#!/bin/bash
+# <mjml><mj-body><mj-section></mj-section></mj-body></mjml>
+
+# cat << EOF | mjml -i
+# $1
+# EOF
+
+
+# STATUS=$(EOF | mjml -i
+# $1
+# EOF
+# )
+# echo $STATUS
---
+++ priv/mjml.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cat << EOF | mjml -i
+$1
+EOF
